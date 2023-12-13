bc5493ee61404228b6873f6b2fa762ad83f77a26
Chris Chen <jk@ChrisdeMac-mini.lan>
2023-12-13 15:09:44 -0800

    向路由组件传递参数三种形式

--- src/pages/home/message/detail/index.jsx
+++ src/pages/home/message/detail/index.jsx
@@ -1,14 +1,29 @@
 import React from 'react';
+import qs from 'querystring-es3'
+
 const detailData=[{ id: '1', content: 'message000000001' },
     { id: '2', content: 'message000000002' },
     { id: '3', content: 'message000000003' }];
 const Detail = (props) => {
     console.log(props)
-    const {id,data}=props.match.params;
-    
+    //1.接收params参数
+    //const {id,data}=props.match.params;
+
+    //2.接收search参数
+    /* const {search}=props.history.location
+    console.log(search)
+    const{id,data}=qs.parse(search.slice(1)) */
+
+    //3.接收state参数
+    const {state}=props.location
+    console.log(state)
+    const{id,data}=state || {}
+
+
+    console.log(id,data)
     const result=detailData.find((obj)=>{
-        return obj.id===id;
-    })
+        return obj.id===id;    
+    }) || {}
 
     return (
         <div>
--- src/pages/home/message/index.jsx
+++ src/pages/home/message/index.jsx
@@ -14,13 +14,29 @@ const Message = () => {
             <ul>
                 {messageArry.map((msgObj) => (
                     <li key={msgObj.id}>
+                       {/*  1.向路由组件传递params参数
                         <NavLink to={`/home/message/detail/${msgObj.id}/${msgObj.data}`}>
+                            {msgObj.data}
+                        </NavLink>&nbsp;&nbsp; */}
+                        {/* 2.向路由组件传递search参数 */}
+                        {/* <NavLink to={`/home/message/detail/?id=${msgObj.id}&data=${msgObj.data}`}>
+                            {msgObj.data}
+                        </NavLink>&nbsp;&nbsp; */}
+                        {/* 3.向路由组件传递state参数 */}
+                        <NavLink to={{pathname:'/home/message/detail/',state:{id:msgObj.id,data:msgObj.data,}}}>
                             {msgObj.data}
                         </NavLink>&nbsp;&nbsp;
                     </li>
                 ))}
                 <hr/>
-                <Route path={`/home/message/detail/:id/:data`} component={Detail} />
+                {/* 1.申明接收params参数
+                <Route path={`/home/message/detail/:id/:data`} component={Detail} /> */}
+                {/* 2.向路由组件传递search参数，无需申明接收 */}
+                {/* <Route path={`/home/message/detail`} component={Detail} />       */}  
+                {/* 3.向路由组件传递state参数，无需申明接收.  */}
+                <Route replace={true} path={`/home/message/detail`} component={Detail} /> 
+                {/* 默认是push，将replace设置为true，则变成无痕迹模式*/ }
+
             </ul>
         
         </div>
